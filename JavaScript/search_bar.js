9169d2082dc202547073293ec9a7fad3cb0c40de
FungSaikit <675703302@qq.com>
2017-10-02 10:15:43 +0800

    first commit

---
+++ JavaScript/search_bar.js
@@ -0,0 +1,28 @@
+window.onscroll = function () {
+    t = document.documentElement.scrollTop;
+    if (t < 100) {
+        if (flag == 1){
+            searchbar.style.borderBottom = "none";
+            searchIMG0.src = "./IMG/search_bar/class_white.png";
+            searchIMG1.src = "./IMG/search_bar/cs_white.png";
+            searchInputIMG0.src = "./IMG/search_bar/search_white.png";
+            searchInputIMG1.src = "./IMG/search_bar/clear_white.png";
+            searchFrame.style.border = "1px solid white";
+            searchInput.className = "searchinput0";
+            flag = 0;
+        }
+        searchbar.style.backgroundColor = "rgba(255, 255, 255, " + (0.1 + t / 400) + ")";
+    } else {
+        if (flag == 0) {
+            searchbar.style.borderBottom = "1px solid lightgray";
+            searchIMG0.src = "./IMG/search_bar/class_black.png";
+            searchIMG1.src = "./IMG/search_bar/cs_black.png";
+            searchInputIMG0.src = "./IMG/search_bar/search_black.png";
+            searchInputIMG1.src = "./IMG/search_bar/clear_black.png";
+            searchFrame.style.border = "1px solid dimgrey";
+            searchInput.className = "searchinput1";
+            flag = 1;
+            searchbar.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
+        }
+    }
+}
